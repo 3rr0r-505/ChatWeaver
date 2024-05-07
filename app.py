ceb780bf66d32249f230debbfd8e0f87b6fc5d86
5pyD3R <96535745+3rr0r-505@users.noreply.github.com>
2024-05-07 22:15:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,56 @@
-import google.generativeai as genai
 import os
 import markdown
 import webbrowser
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Get API key from environment variables
-API_KEY = os.getenv('GEMINI_API_KEY')
-genai.configure(api_key = API_KEY)
+# MongoDB connection
+MONGODB_URI = os.getenv('MONGODB_URI')
+client = MongoClient(MONGODB_URI)
+db = client.get_default_database()
+chats_collection = db.get_collection('chats')
+
+# Your existing code
+import google.generativeai as genai
+genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 model = genai.GenerativeModel('gemini-1.0-pro-latest')
 
-# Initialize Flask app
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('chat.html')
+    # Load previous chats from MongoDB
+    previous_chats = list(chats_collection.find())
+    
+    # Convert bot responses to markdown format
+    for chat in previous_chats:
+        chat['bot_response'] = markdown.markdown(chat['bot_response'])
+    
+    return render_template('chat.html', previous_chats=previous_chats)
+
 
 @app.route("/get", methods=["POST"])
 def chat():
-    # Get user message from request
     user_message = request.form["msg"]
-    return get_chat_response(user_message)
+    bot_response_markdown , raw_response = get_chat_response(user_message)  # Get the markdown-formatted response
+    
+    # print(bot_response_markdown)
+    # Save the chat to MongoDB with markdown-formatted response
+    chats_collection.insert_one({"user_message": user_message, "bot_response": raw_response})
+    
+    return bot_response_markdown
 
-# Function to generate chatbot response in markdown format
 def get_chat_response(question):
-    # Send user message to the model and receive response
     response = model.generate_content(question)
-    # Convert the response text to markdown format
+
     markdown_response = markdown.markdown(response.text)
-    # Return the markdown formatted response
-    return markdown_response
+    return markdown_response, response.text
 
 
-# Main entry point of the application
 if __name__ == '__main__':
     webbrowser.open('http://127.0.0.1:8000')
     app.run(host='127.0.0.1', port=8000)
